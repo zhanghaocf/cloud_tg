dd4f48d82908e9de1b02f4c63dad229e529893c2
‘grootZhang’ <zhaicf37@163.com>
2021-12-21 17:16:33 +0800

    feat-流水线

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 import os
 
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
   return "欢迎使用微信云托管"
+
+@app.route('/test')
+def test():
+  return jsonify({success:True,msg:'NIUBI'})
 
 if __name__ == '__main__':
   app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT',80)))
